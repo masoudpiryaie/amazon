7d2113d3e732cf673c641f3436a5c0de6c61c7e8
masoud <fornt.react.dev@gmail.com>
2025-02-08 17:04:47 +0330

    aa

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,4 +1,4 @@
-import { EllipsisVertical } from 'lucide-react'
+import { EllipsisVertical } from "lucide-react";
 import {
   Sheet,
   SheetContent,
@@ -6,32 +6,32 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from '@/components/ui/sheet'
-import CartButton from './cart-button'
-import UserButton from './user-button'
-import ThemeSwitcher from './theme-switcher'
-import LanguageSwitcher from './language-switcher'
-import { useTranslations } from 'next-intl'
+} from "@/components/ui/sheet";
+import CartButton from "./cart-button";
+import UserButton from "./user-button";
+import ThemeSwitcher from "./theme-switcher";
+import LanguageSwitcher from "./language-switcher";
+import { useTranslations } from "next-intl";
 
 const Menu = ({ forAdmin = false }: { forAdmin?: boolean }) => {
-  const t = useTranslations()
+  const t = useTranslations();
   return (
-    <div className='flex justify-end'>
-      <nav className='md:flex gap-3 hidden w-full'>
-        <LanguageSwitcher />
+    <div className="flex justify-end">
+      <nav className="md:flex gap-3 hidden w-full">
+        {/* <LanguageSwitcher /> */}
         <ThemeSwitcher />
         <UserButton />
         {forAdmin ? null : <CartButton />}
       </nav>
-      <nav className='md:hidden'>
+      <nav className="md:hidden">
         <Sheet>
-          <SheetTrigger className='align-middle header-button'>
-            <EllipsisVertical className='h-6 w-6' />
+          <SheetTrigger className="align-middle header-button">
+            <EllipsisVertical className="h-6 w-6" />
           </SheetTrigger>
-          <SheetContent className='bg-black text-white  flex flex-col items-start  '>
-            <SheetHeader className='w-full'>
-              <div className='flex items-center justify-between '>
-                <SheetTitle className='  '>{t('Header.Site Menu')}</SheetTitle>
+          <SheetContent className="bg-black text-white  flex flex-col items-start  ">
+            <SheetHeader className="w-full">
+              <div className="flex items-center justify-between ">
+                <SheetTitle className="  ">{t("Header.Site Menu")}</SheetTitle>
                 <SheetDescription></SheetDescription>
               </div>
             </SheetHeader>
@@ -43,7 +43,7 @@ const Menu = ({ forAdmin = false }: { forAdmin?: boolean }) => {
         </Sheet>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
